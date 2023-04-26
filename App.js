87e991f78151ccf23a388900b45fbe07b83c7a03
Kallin Messerer <khmesserer@gmail.com>
2023-04-25 19:39:09 -0500

    Fixed history key warning

--- App.js
+++ App.js
@@ -42,181 +42,180 @@ function openDatabase() {
 
 const db = openDatabase();
 
-function Results() {
-  const [results, setResults] = useState(null);
-
-  useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        `select * from calculations order by id desc;`,
-        null,
-        (_, { rows: { _array } }) => setResults(_array)
-      );
-    });
-  }, []);
-
-  if (results === null || results.length === 0) {
-    return null;
-  }
-
-  return (
-    <View> 
-      {results.map(({ loanAmount, result, calcDate }) => (
-        <Result loanAmount={loanAmount} result={result} calcDate={calcDate} />
-      ))}
-    </View>
-  )
-}
-
-function Result({loanAmount, result, calcDate}){
-  return (
-    <View style={styles.historyRow}>
-      <Text style={styles.historyDate}>{calcDate}</Text>
-      <Text style={styles.historyAmount}>{formatCurrency(loanAmount)}</Text>
-      <Text style={styles.historyResult}>{formatCurrency(result)}</Text>
-    </View>
-  );
-}
-
-function HomeScreen({ navigation }) {
-  const [loanAmount, setLoanAmount] = useState(null);
-  const [interestRate, setInterestRate] = useState(null);
-  const [loanLength, setLoanLength] = useState(null);
-  const [forceUpdate, forceUpdateId] = useForceUpdate();
-  const result = null;
-
-  function isValid() {
-    if(isNaN(loanAmount)){
-      alert("Loan Amount must be only numeric characters");
-      return false;
-    }else if(loanAmount < 50000){
-      alert("Loan Amount must be greater than $50,000");
-      return false;
-    }else if(isNaN(interestRate)){
-      alert("Interest Rate must be whole number");
-      return false;
-    }else if(interestRate <= 0 || interestRate > 100){
-      alert("Interest Rate must be greater than 0% and lesser than 100%");
-      return false;
-    }else if(isNaN(loanLength)){
-      alert("Loan Length must be a whole number");
-      return false;
-    }else if(loanLength <= 0 || loanLength > 30){
-      alert("Loan Length must be greater than 0 and lesser than or equal to 30 years");
-      return false;
-    }
-    let i = (interestRate*0.01)/12;
-    let n = loanLength*12;
-    const result = loanAmount * (i * (1 + i) ** n) / ((1 + i) ** n - 1);
-    let calcResult = {
-      amount: formatCurrency(loanAmount),
-      interestRate: formatPercentage(interestRate),
-      length: loanLength + " Years",
-      result: formatCurrency(result)
-    };
-
-    try {
-      add(result);
-      navigation.navigate('Result', {result: calcResult});
-    } catch (error) {
-      alert('There was an error while saving the data');
-    }
-    return true;
-  }
-
-  useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "create table if not exists calculations (id integer primary key not null, loanAmount real, result real, calcDate real);"
-      );
-    });
-  }, []);
-
-  const add = (result) => {
-    db.transaction(
-      (tx) => {
-        tx.executeSql("insert into calculations (loanAmount, result, calcDate) values (?, ?, date('now'))", [loanAmount, result]);
-        tx.executeSql(`select id, loanAmount, result, date(calcDate) as calcDate from calculations order by id desc;`, [], (_, { rows }) =>
-          console.log(JSON.stringify(rows))
-        );
-      },
-      null,
-      forceUpdate
-    );
-  };
-
-  return (
-    <View style={styles.container}>
-      <Image style={styles.elementContainer} source={homeImage} />
-      <View style={styles.elementContainer}>
-        <Text style={styles.resultLabel}>Loan Amount</Text>
-        <TextInput style={styles.input} placeholder={"Loan Amount"}
-          onChangeText={newText => setLoanAmount(newText)}
-        />
-        <Text style={styles.resultLabel}>Interest Rate as Percent</Text>
-        <TextInput style={styles.input} placeholder={"Interest Rate"}
-          onChangeText={newText => setInterestRate(newText)}
-         />
-         <Text style={styles.resultLabel}>Loan Length in Years</Text>
-        <TextInput style={styles.input} placeholder={"Loan Length in Years"}
-          onChangeText={newText => setLoanLength(newText)}
-         />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Pressable 
-          onPress={() => isValid()}
-        >
-          <Text style={styles.buttonText}>Calculate</Text>
-        </Pressable>
-      </View>
-      <View style={styles.linkContainer}>
-        <Button
-          title="Shop for Homes on Zillow"
-          onPress={() => WebBrowser.openBrowserAsync('https://www.zillow.com')}
-          style={styles.button}
-        />
-      </View>
-    </View>
-  );
-}
-
-function ResultScreen({ route, navigation }) {
-  const {result} = route.params;
-  const [forceUpdate, forceUpdateId] = useForceUpdate();
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.resultHeader}>
-        <Text style={styles.title}>Results</Text>
-      </View>
-      <View style={styles.resultContainer}>
-        <Text style={styles.resultLabel}>Loan Amount</Text>
-        <TextInput value={result.amount} editable={false} style={styles.input} />
-        <Text style={styles.resultLabel}>Interest Rate</Text>
-        <TextInput value={result.interestRate} editable={false} style={styles.input} />
-        <Text style={styles.resultLabel}>Length of Loan</Text>
-        <TextInput value={result.length} editable={false} style={styles.input} />
-        <Text style={styles.resultLabel}>Monthly Payment Amount</Text>
-        <TextInput value={result.result} editable={false} style={styles.input} />
-      </View>
-      <View style={styles.historyContainer}>
-        <Text style={styles.historyHeader}>Calculation History</Text>
-        <View style={styles.historyRow}>
-          <Text style={styles.historyDate}>Date</Text>
-          <Text style={styles.historyAmount}>Amount</Text>
-          <Text style={styles.historyResult}>Result</Text>
-        </View>
-        <ScrollView>
-          <Results key={forceUpdateId}/>
-        </ScrollView>
-      </View>
-    </View>
-  );
-}
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [forceUpdate, forceUpdateId] = useForceUpdate();
+
+  function Results({ keyId }) {
+    const [results, setResults] = useState(null);
+
+    useEffect(() => {
+      db.transaction((tx) => {
+        tx.executeSql(
+          `select * from calculations order by id desc;`,
+          null,
+          (_, { rows: { _array } }) => setResults(_array)
+        );
+      });
+    }, []);
+
+    if (results === null || results.length === 0) {
+      return null;
+    }
+
+    return (
+      <View key={keyId}> 
+        {results.map(({ id, loanAmount, result, calcDate }) => (
+          <Result key={id} id={id} loanAmount={loanAmount} result={result} calcDate={calcDate} />
+        ))}
+      </View>
+    )
+  }
+
+  function Result({id, loanAmount, result, calcDate}){
+    return (
+      <View style={styles.historyRow} key={id}>
+        <Text style={styles.historyDate}>{calcDate}</Text>
+        <Text style={styles.historyAmount}>{formatCurrency(loanAmount)}</Text>
+        <Text style={styles.historyResult}>{formatCurrency(result)}</Text>
+      </View>
+    );
+  }
+
+  function HomeScreen({ navigation }) {
+    const [loanAmount, setLoanAmount] = useState(null);
+    const [interestRate, setInterestRate] = useState(null);
+    const [loanLength, setLoanLength] = useState(null);
+  
+    function isValid() {
+      if(isNaN(loanAmount)){
+        alert("Loan Amount must be only numeric characters");
+        return false;
+      }else if(loanAmount < 50000){
+        alert("Loan Amount must be greater than $50,000");
+        return false;
+      }else if(isNaN(interestRate)){
+        alert("Interest Rate must be whole number");
+        return false;
+      }else if(interestRate <= 0 || interestRate > 100){
+        alert("Interest Rate must be greater than 0% and lesser than 100%");
+        return false;
+      }else if(isNaN(loanLength)){
+        alert("Loan Length must be a whole number");
+        return false;
+      }else if(loanLength <= 0 || loanLength > 30){
+        alert("Loan Length must be greater than 0 and lesser than or equal to 30 years");
+        return false;
+      }
+      let i = (interestRate*0.01)/12;
+      let n = loanLength*12;
+      const result = loanAmount * (i * (1 + i) ** n) / ((1 + i) ** n - 1);
+      let calcResult = {
+        amount: formatCurrency(loanAmount),
+        interestRate: formatPercentage(interestRate),
+        length: loanLength + " Years",
+        result: formatCurrency(result)
+      };
+  
+      try {
+        add(result);
+        navigation.navigate('Result', {result: calcResult});
+      } catch (error) {
+        alert('There was an error while saving the data');
+      }
+      return true;
+    }
+  
+    useEffect(() => {
+      db.transaction((tx) => {
+        tx.executeSql(
+          "create table if not exists calculations (id integer primary key not null, loanAmount real, result real, calcDate real);"
+        );
+      });
+    }, []);
+  
+    const add = (result) => {
+      db.transaction(
+        (tx) => {
+          tx.executeSql("insert into calculations (loanAmount, result, calcDate) values (?, ?, date('now'))", [loanAmount, result]);
+          tx.executeSql(`select id, loanAmount, result, date(calcDate) as calcDate from calculations order by id desc;`, [], (_, { rows }) =>
+            console.log(JSON.stringify(rows))
+          );
+        },
+        null,
+        forceUpdate
+      );
+    };
+  
+    return (
+      <View style={styles.container}>
+        <Image style={styles.elementContainer} source={homeImage} />
+        <View style={styles.elementContainer}>
+          <Text style={styles.resultLabel}>Loan Amount</Text>
+          <TextInput style={styles.input} placeholder={"Loan Amount"}
+            onChangeText={newText => setLoanAmount(newText)}
+          />
+          <Text style={styles.resultLabel}>Interest Rate as Percent</Text>
+          <TextInput style={styles.input} placeholder={"Interest Rate"}
+            onChangeText={newText => setInterestRate(newText)}
+           />
+           <Text style={styles.resultLabel}>Loan Length in Years</Text>
+          <TextInput style={styles.input} placeholder={"Loan Length in Years"}
+            onChangeText={newText => setLoanLength(newText)}
+           />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Pressable 
+            onPress={() => isValid()}
+          >
+            <Text style={styles.buttonText}>Calculate</Text>
+          </Pressable>
+        </View>
+        <View style={styles.linkContainer}>
+          <Button
+            title="Shop for Homes on Zillow"
+            onPress={() => WebBrowser.openBrowserAsync('https://www.zillow.com')}
+            style={styles.button}
+          />
+        </View>
+      </View>
+    );
+  }
+
+  function ResultScreen({ route, navigation }) {
+    const {result} = route.params;
+  
+    return (
+      <View style={styles.container}>
+        <View style={styles.resultHeader}>
+          <Text style={styles.title}>Results</Text>
+        </View>
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultLabel}>Loan Amount</Text>
+          <TextInput value={result.amount} editable={false} style={styles.input} />
+          <Text style={styles.resultLabel}>Interest Rate</Text>
+          <TextInput value={result.interestRate} editable={false} style={styles.input} />
+          <Text style={styles.resultLabel}>Length of Loan</Text>
+          <TextInput value={result.length} editable={false} style={styles.input} />
+          <Text style={styles.resultLabel}>Monthly Payment Amount</Text>
+          <TextInput value={result.result} editable={false} style={styles.input} />
+        </View>
+        <View style={styles.historyContainer}>
+          <Text style={styles.historyHeader}>Calculation History</Text>
+          <View style={styles.historyRow}>
+            <Text style={styles.historyDate}>Date</Text>
+            <Text style={styles.historyAmount}>Amount</Text>
+            <Text style={styles.historyResult}>Result</Text>
+          </View>
+          <ScrollView>
+            <Results key={forceUpdateId} keyId={forceUpdateId}/>
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator 
